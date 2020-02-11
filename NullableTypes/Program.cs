e82c78a213282086bbb123c67fe19967168fd408
David Price <dwerkprice@gmail.com>
2020-02-11 15:32:54 -0500

    Add project files.

---
+++ NullableTypes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NullableTypes
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            var math = new Major {Id = 1, Description = "Math",MinSAT = 900};
+
+            var dave = new 
+                ID=1,
+                Name="dave",
+            GPA = 2.9,
+            SAT = 1000
+            MajorID = math.id
+                
+                n
+            }
+            }
+        }
+    }
+}
---
+++ NullableTypes/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NullableTypes
+{
+    public class Student {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double GPA { get; set; }
+        public int? SAT { get; set; }
+
+      puclic int? MajorId { get; set; }
+      public Major major { get; set; }  
+    }
+}
